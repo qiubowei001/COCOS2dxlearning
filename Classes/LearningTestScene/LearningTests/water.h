d570290bc86a2dd98e3df85f7a65a5f29e2589fb
qiubowei <qiubowei001@qq.com>
2015-01-21 12:29:39 +0800

    resource

--- Classes/LearningTestScene/LearningTests/water.h
+++ Classes/LearningTestScene/LearningTests/water.h
@@ -1,10 +1,30 @@
 #include "cocos2d.h"
 #include "LearningTest.h"
 
+
+#ifndef __WATER_EFFECT_SPRITE_H__
+#define __WATER_EFFECT_SPRITE_H__
+
+USING_NS_CC;
+
+class WaterEffectSprite : public Sprite {
+public:
+	static WaterEffectSprite* create(const char *pszFileName, int ShaderIndex);
+public:
+	bool initWithTexture(Texture2D* texture, const Rect&  rect);
+	void initGLProgram(int ShaderIndex);
+};
+
+#endif
+
 class water : public LearningTest
 {
+private:
+	WaterEffectSprite* mWater;
 public:
 	virtual std::string subtitle() const override;
 	virtual void onEnter() override;
 	void removeThis();
+	void nextCallback(Ref* sender);
 };
+
---
+++ Resources/Shaders/WaterEffect3.fsh
@@ -0,0 +1,19 @@
+varying vec4 v_fragmentColor;
+varying vec2 v_texCoord;
+
+float waveHeight(vec2p){
+    float timeFactor=4.0;
+    float texFactor=12.0;
+    float ampFactor=0.01;
+    float dist=length(p);
+    return cos(CC_Time.y*timeFactor+dist*texFactor)*ampFactor;
+}
+
+void main(){
+    //convertto[-1,1]
+    vec2 p=-1.0+2.0*v_texCoord;
+    vec2 normal=normalize(p);
+    //offset texcoord along distdirection
+    v_texCoord+=normal*waveHeight(p);
+    gl_FragColor=texture2D(CC_Texture0,v_texCoord)*v_fragmentColor;
+}
